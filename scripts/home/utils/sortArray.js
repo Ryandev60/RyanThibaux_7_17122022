eb745bd7f2801471e8b0dd5eb97db15beb77f3aa
Ryan Thibaux <rthibaux@sogec-marketing.fr>
2022-12-21 21:25:14 +0100

    sort ingredient

--- scripts/home/utils/sortArray.js
+++ scripts/home/utils/sortArray.js
@@ -1,33 +1,59 @@
 export let arrayOfSearchRecipe = [];
+export let arrayOfSearchIngredients = [];
 
-
-const sortArray = (array) => {
-    arrayOfSearchRecipe = []
-
-    recipes.forEach((recipe) => {
-
-        if (
-            recipe.name.toLowerCase().includes(searchBar.value.toLowerCase()) &&
-            arrayOfSearchRecipe.indexOf(recipe) == -1
-        ) {
-            arrayOfSearchRecipe.push(recipe);
-        }
-        if (
-            recipe.description.toLowerCase().includes(searchBar.value.toLowerCase()) &&
-            arrayOfSearchRecipe.indexOf(recipe) == -1
-        ) {
-            arrayOfSearchRecipe.push(recipe);
-        }
-        recipe.ingredients.forEach((ingredient) => {
+const sortArray = (array, filter) => {
+    if (filter === 'search-bar') {
+        let tableauCopy = [];
+        tableauCopy = array;
+        arrayOfSearchRecipe = [];
+        console.log(tableauCopy);
+        array.forEach((recipe) => {
             if (
-                ingredient.ingredient.toLowerCase().includes(searchBar.value.toLowerCase()) &&
+                recipe.name.toLowerCase().includes(searchBar.value.toLowerCase()) &&
                 arrayOfSearchRecipe.indexOf(recipe) == -1
             ) {
                 arrayOfSearchRecipe.push(recipe);
             }
+            if (
+                recipe.description.toLowerCase().includes(searchBar.value.toLowerCase()) &&
+                arrayOfSearchRecipe.indexOf(recipe) == -1
+            ) {
+                arrayOfSearchRecipe.push(recipe);
+            }
+            recipe.ingredients.forEach((ingredient) => {
+                if (
+                    ingredient.ingredient.toLowerCase().includes(searchBar.value.toLowerCase()) &&
+                    arrayOfSearchRecipe.indexOf(recipe) === -1
+                ) {
+                    arrayOfSearchRecipe.push(recipe);
+                }
+            });
         });
-    });
-    console.log(arrayOfSearchRecipe);
+    }
+
+    if (filter === 'ingredients') {
+        console.log(arrayOfSearchIngredients);
+        console.log('test copie');
+        console.log(arrayOfSearchRecipe);
+        arrayOfSearchIngredients = [];
+        array.forEach((recipe) => {
+            let isSearch = false;
+            recipe.ingredients.forEach((ingredient) => {
+                if (
+                    ingredient.ingredient.toLowerCase().includes(filterIngredients.value.toLowerCase()) &&
+                    arrayOfSearchIngredients.indexOf(recipe) === -1
+                ) {
+                    isSearch = true;
+                }
+            });
+
+            if (isSearch === true) {
+                arrayOfSearchIngredients.push(recipe);
+                // arrayOfSearchIngredients.splice(1, arrayOfSearchIngredients.indexOf(recipe))
+            } else {
+            }
+        });
+    }
 
 };
 
--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ import getIngredients from './home/components/getIngredients.js';
 import getAppliance from './home/components/getAppliance.js';
 import getUstensils from './home/components/getUstensils.js';
 import recipeCard from './home/components/recipeCard.js';
-import sortArray from './home/utils/sortArray.js';
+import sortArray, { arrayOfSearchIngredients } from './home/utils/sortArray.js';
 import { arrayOfSearchRecipe } from './home/utils/sortArray.js';
 
 const displayData = () => {
@@ -12,13 +12,25 @@ const displayData = () => {
     recipeCardContainer.innerHTML = recipeCard(recipes);
     ingredientsList.innerHTML = getIngredients();
 
-    searchBar.addEventListener("input",() => {
+    searchBar.addEventListener('input', () => {
         if (searchBar.value.length > 2) {
-            sortArray();
-            console.log(arrayOfSearchRecipe);
+            sortArray(recipes, 'search-bar');
             recipeCardContainer.innerHTML = recipeCard(arrayOfSearchRecipe);
         }
-    
+        else if (searchBar.value.length === 2){
+             recipeCardContainer.innerHTML = recipeCard(recipes);
+        }
+    });
+
+    filterIngredients.addEventListener('input', () => {
+        if (searchBar.value.length > 2) {
+            sortArray(arrayOfSearchRecipe, 'ingredients');
+            recipeCardContainer.innerHTML = recipeCard(arrayOfSearchIngredients);
+        }
+        if (searchBar.value.length < 3) {
+            sortArray(recipes, 'ingredients');
+            recipeCardContainer.innerHTML = recipeCard(arrayOfSearchIngredients);
+        }
     });
 };
 
